4b0ce062b87d63631841b22f06d655a295246d02
0verGeek <3298866863@qq.com>
2026-07-31 17:59:48 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.default
+                  {
+                    home-manager = {
+                      useGlobalPkgs = true;
+                      useUserPackages = true;
+                      extraSpecialArgs = { inherit inputs; }; #If you want access to inputs in your home.nix
+                      users.camuss = ./home.nix; # replace <USERNAME> with your actual username
+                    };
+                  }
       ];
     };
   };
