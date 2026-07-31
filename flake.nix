57970bfa8ea1a79aad97a649d03b311c682df5a1
0verGeek <3298866863@qq.com>
2026-07-31 17:20:54 +0800

    初始 FLakes 配置

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "我的 NixOS 配置";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
